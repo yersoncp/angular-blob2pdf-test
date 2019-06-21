b49ca8fdb42821f259005d1c06590f384e927e3f
yersoncp <yerson.rc@gmail.com>
2019-06-21 15:52:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-blob2pdf-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-blob2pdf-test)
---
+++ src/app/hello.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'hello',
+  template: `<a [href]="fileUrl" download="file.pdf">DownloadFile</a>`,
+  styles: [`h1 { font-family: Lato; }`]
+})
+export class HelloComponent  {
+  @Input() name: string;
+  public fileUrl;
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private http:HttpClient
+  ) {}
+
+  ngOnInit(){
+    this.getPdf().subscribe(e => {
+      console.log(e);
+      //var file = new Blob([e['blob']], {type: 'application/pdf'});
+      this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(e));
+    })
+  }
+
+  private getPdf(){
+    console.log('call getPdf()');
+    //return this.http.get('https://us-central1-element-services.cloudfunctions.net/pdf/paymentDocument');
+
+    return this.http.post('https://us-central1-element-services.cloudfunctions.net/pdf/paymentDocument', { hi: }, { responseType: 'blob' });
+
+  }
+
+}
